fe4242c91dea1f98b95697d85dc99fae38e915b3
Max Vorontsov <joistick11@gmail.com>
2017-03-22 19:55:02 +0300

    add

--- haotic_driver/main.py
+++ haotic_driver/main.py
@@ -1,5 +1,22 @@
+import random
+
 from echo_sensor import has_obstacle
-from motors_controller import run_forward, full_stop
+from motors_controller import run_forward, full_stop, turn_right, turn_left
+import pyupm_buzzer as upmBuzzer
+
+buzzer = upmBuzzer.Buzzer(9)
+
+
+def make_bip():
+    buzzer.playSound(upmBuzzer.MI, 100000)  # delay in microseconds
+
+
+def make_decision():
+    if random.randint(0, 1):
+        turn_right()
+    else:
+        turn_left()
+
 
 if __name__ == '__main__':
     try:
@@ -8,6 +25,9 @@ if __name__ == '__main__':
                 run_forward(1)
             else:
                 full_stop()
+                make_decision()
     except KeyboardInterrupt:
         full_stop()
         exit(1)
+
+
