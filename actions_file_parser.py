c71db715b1347db5d159ac6bd164326e1a4b1706
desio05 <gcinbax@gmail.com>
2017-03-23 18:14:51 +0300

    read actions from file + test

---
+++ actions_file_parser.py
@@ -0,0 +1,12 @@
+def parse_actions_from_file(path_to_file):
+    actions = []
+    with open(path_to_file) as path_file:
+        for row in path_file:
+            direction, step_length = row.split(" ")
+            step_length = step_length.replace("\n", '')
+            actions.append((direction, int(step_length)))
+    return actions
+
+
+if __name__ == '__main__':
+    print(parse_actions_from_file("actions_txt/square_and_back_to_start"))
